ab104fe69e2f0d00f6cb61fbb5a3166becb7199f
pruthvi10 <pruthvimohanlal10@gmail.com>
2021-01-25 20:32:54 +0000

    Updates

---
+++ precache-manifest.94908cf912d6e87530f6db5f74079ca1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1943ca2b620c1bb53978b09c7dc8f80",
+    "url": "/spatepate/index.html"
+  },
+  {
+    "revision": "bc2c48c861d4c01d919c",
+    "url": "/spatepate/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e06b5429830e959aa2e4",
+    "url": "/spatepate/static/css/main.a6e2a4aa.chunk.css"
+  },
+  {
+    "revision": "bc2c48c861d4c01d919c",
+    "url": "/spatepate/static/js/2.8b84c0f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/spatepate/static/js/2.8b84c0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06b5429830e959aa2e4",
+    "url": "/spatepate/static/js/main.46175e6d.chunk.js"
+  },
+  {
+    "revision": "3454061c526dd87c3877",
+    "url": "/spatepate/static/js/runtime-main.0e0a54b8.js"
+  },
+  {
+    "revision": "39b330d277cbbf46cf2d2721b48c57d6",
+    "url": "/spatepate/static/media/bitdegree.39b330d2.jpg"
+  },
+  {
+    "revision": "11d911b60e284d1c8dfd5f7733419eea",
+    "url": "/spatepate/static/media/code.11d911b6.jpg"
+  },
+  {
+    "revision": "5b78666dc0316b719a8fe25e7ba51bb5",
+    "url": "/spatepate/static/media/codecademy.5b78666d.png"
+  },
+  {
+    "revision": "ad0df3acc706d277bba44122bbe2238b",
+    "url": "/spatepate/static/media/coding.ad0df3ac.png"
+  },
+  {
+    "revision": "f612e970ed9621ceabf089f967540e81",
+    "url": "/spatepate/static/media/coursera.f612e970.png"
+  },
+  {
+    "revision": "cc696f6d3b581b3e6054f0b36a0146f9",
+    "url": "/spatepate/static/media/edx.cc696f6d.jpg"
+  },
+  {
+    "revision": "41c83aeffd99287a6a162428682c50b0",
+    "url": "/spatepate/static/media/elon.41c83aef.jpg"
+  },
+  {
+    "revision": "ad8e576f5832038e00840a2b604349e9",
+    "url": "/spatepate/static/media/elon2.ad8e576f.jpg"
+  },
+  {
+    "revision": "96863a3ab3f07490d609a031a6a00db3",
+    "url": "/spatepate/static/media/founder.96863a3a.jpg"
+  },
+  {
+    "revision": "48a892e5d76bf282c5bfc825797af9e6",
+    "url": "/spatepate/static/media/freecodecamp.48a892e5.png"
+  },
+  {
+    "revision": "fc65c0144479cc21068287b2479954a7",
+    "url": "/spatepate/static/media/person.fc65c014.svg"
+  },
+  {
+    "revision": "2ccaca302010c2b8e9fa82111bf1ba6b",
+    "url": "/spatepate/static/media/sololearn.2ccaca30.png"
+  },
+  {
+    "revision": "0872a03fdb2c7f32e26d19ff10c5f236",
+    "url": "/spatepate/static/media/udemy.0872a03f.jpg"
+  },
+  {
+    "revision": "d94504b6b9267e05b70e55f62551eed6",
+    "url": "/spatepate/static/media/w3schools.d94504b6.png"
+  }
+]);
