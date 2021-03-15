59743eb14bdfdbaa419c92d5de7b8ef0ea5e8984
pruthvi10 <pruthvimohanlal10@gmail.com>
2021-03-15 16:03:25 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,125 @@ import React from "react";
 // Page Styling and Bootstrap Import
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 // Page components import
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
-// Components Imports
+// Importing all pages below
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import PageDeleted from "./components/Pagedeleted";
+
+// languages
+import Java from "./components/subpages/Java";
+import Javascript from "./components/subpages/Javascript";
+import Python from "./components/subpages/Python";
+import Developers from "./components/subpages/Developers";
+import Resource from "./components/subpages/Resources";
+import Books from "./components/Book";
+import Programming from "./components/Programming";
+import Csharp from "./components/subpages/Csharp";
+import Cplusplus from "./components/subpages/Cplusplus";
+import Swift from "./components/subpages/Swift";
+import Typescript from "./components/subpages/Typescript";
+import GoogleBooks from "./components/GoogleBooks";
 
 function App() {
   return (
     <div className="App">
       {/* MAIN PAGE LAYOUT */}
       <NavBar />
+      <Router>
+        {/* Page redirection. */}
+        <Switch>
+          {/* Home page */}
+          <Route path="/spatepate/" exact component={Home}></Route>
+
+          {/* About page */}
+          <Route path="/spatepate/about-us" exact component={About}></Route>
+
+          {/* Contact us page */}
+          <Route path="/spatepate/contact-us" exact component={Contact}></Route>
+
+          {/* Routing the programming page */}
+          <Route
+            path="/spatepate/programming"
+            exact
+            component={Programming}
+          ></Route>
+
+          {/* Routing the languages */}
+          <Route
+            path="/spatepate/language/learn-java"
+            exact
+            component={Java}
+          ></Route>
+
+          {/* Javascript page */}
+          <Route
+            path="/spatepate/language/learn-javascript"
+            component={Javascript}
+          ></Route>
+
+          {/* Python page */}
+          <Route
+            path="/spatepate/language/learn-python"
+            component={Python}
+          ></Route>
+
+          {/* C# page */}
+          <Route
+            path="/spatepate/language/learn-Csharp"
+            component={Csharp}
+          ></Route>
+
+          {/* C++ page */}
+          <Route
+            path="/spatepate/language/learn-cplusplus"
+            component={Cplusplus}
+          ></Route>
+
+          {/* Swift page */}
+          <Route
+            path="/spatepate/language/learn-swift"
+            component={Swift}
+          ></Route>
+
+          {/* Kotlin page */}
+          <Route path="/spatepate/language/learn-kotlin">
+            <p className="p-1 ">
+              vist this page later, this page is under construction.
+            </p>
+            <h2>Kotlin page coming here soon...</h2>
+          </Route>
+
+          {/* Typescript page */}
+          <Route
+            path="/spatepate/language/learn-typescript"
+            component={Typescript}
+          ></Route>
+
+          {/* All Developers page */}
+          <Route path="/spatepate/developers">
+            <Developers />
+          </Route>
+
+          {/* course resources page. */}
+          <Route path="/spatepate/resources" component={Resource}></Route>
+
+          {/* course book api page  . */}
+          <Route path="spatepate/books" component={Books}></Route>
+
+          {/* course book api page  . */}
+          <Route path="spatepate/googlebooks" component={GoogleBooks}></Route>
+
+          {/* 404 page */}
+          <Route component={PageDeleted} />
+        </Switch>
+      </Router>
       <Footer />
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,164 +1,60 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Navbar,
-  Nav,
-  NavItem
-} from "react-bootstrap";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../App.css";
 
-// Importing all pages below
-import About from "./About";
-import Contact from "./Contact";
-import Home from "./Home";
-import PageDeleted from "./Pagedeleted";
-
-// languages
-import Java from "./subpages/Java";
-import Javascript from "./subpages/Javascript";
-import Python from "./subpages/Python";
-import Developers from "./subpages/Developers";
-import Resource from "./subpages/Resources";
-import Books from "./Book";
-import Programming from "./Programming";
-import Csharp from "./subpages/Csharp"
-import Cplusplus from "./subpages/Cplusplus"
-import Swift from "./subpages/Swift"
-import Typescript from "./subpages/Typescript"
-
-
-
-
-// Temp files
-import GoogleBooks from "./GoogleBooks"
-
-
-// Navbar Function, with all the dropdowns and links. 
-const NavBar = props => {
-
+// Navbar Function, with all the dropdowns and links.
+const NavBar = (props) => {
   return (
     // page router. React bootstrap navbar.
-    <Router>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        className="navbar-custom"
-        variant="dark"
-        // sticky="top"
-      >
-        <Navbar.Brand href="/spatepate/" className="navbar-brand">spatepate</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <NavItem>   <Link className="nav-link" to="/spatepate/">Home</Link> </NavItem>
-            <NavItem>   <Link className="nav-link" to="/spatepate/programming"><i className="fa fa-code"></i> Programming</Link> </NavItem>
-            <NavItem>   <Link className="nav-link" to="/spatepate/developers">Developers</Link> </NavItem>
-            <NavItem>   <Link className="nav-link" to="/spatepate/about-us">About</Link> </NavItem>
-            <NavItem>   <Link className="nav-link" to="/spatepate/contact-us">Contact</Link> </NavItem>
-          </Nav>
-          <Nav>
-          <NavItem>   <Link className="nav-link" to="/spatepate/resources">Resources</Link> </NavItem>
-            <Nav.Link eventKey={2} href="https://www.reddit.com/r/dankmemes/" target="_blank"  rel="noopener noreferrer">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
 
-
-        {/* Page redirection. */}
-      <Switch>
-        {/* Home page */}
-        <Route exact path="/spatepate/">
-          <Home />
-        </Route>
-
-        {/* About page */}
-        <Route exact path="/spatepate/about-us">
-          <About />
-        </Route>
-
-        {/* Contact us page */}
-        <Route exact path="/spatepate/contact-us">
-          <Contact />
-        </Route>
-
-         {/* Routing the programming page */}
-         <Route exact path="/spatepate/programming">
-         <Programming />
-        </Route>
-
-        {/* Routing the languages */}
-        <Route exact path="/spatepate/language/learn-java">
-          <Java />
-        </Route>
-
-        {/* Javascript page */}
-        <Route exact path="/spatepate/language/learn-javascript">
-          <Javascript />
-        </Route>
-
-        {/* Python page */}
-        <Route exact path="/spatepate/language/learn-python">
-          <Python />
-        </Route>
-        
-        {/* C# page */}
-        <Route exact path="/spatepate/language/learn-Csharp">
-          <Csharp/>
-        </Route>
-
-        {/* C++ page */}
-        <Route exact path="/spatepate/language/learn-cplusplus">
-          <Cplusplus />
-        </Route>
-
-        {/* Swift page */}
-        <Route exact path="/spatepate/language/learn-swift">
-          <Swift />
-        </Route>
-
-        {/* Kotlin page */}
-        <Route exact path="/spatepate/language/learn-kotlin">
-          <p className="p-1 ">vist this page later, this page is under construction.</p>
-          <h2>Kotlin page coming here soon...</h2>
-        </Route>
-
-        {/* Typescript page */}
-        <Route path="/spatepate/language/learn-typescript">
-          <Typescript />
-        </Route>
-
-        {/* All Developers page */}
-        <Route exact path="/spatepate/developers">
-          <Developers />
-        </Route>
-        
-        {/* course resources page. */}
-        <Route exact path="/spatepate/resources">
-          <Resource/>
-        </Route>
-
-        {/* course book api page  . */}
-        <Route exact path="spatepate/books">
-          <Books/>
-        </Route>
-
-       {/* course book api page  . */}
-       <Route exact path="spatepate/googlebooks">
-          <GoogleBooks/>
-        </Route>
-
-        {/* 404 page */}
-        <Route component={PageDeleted}/>
-      </Switch>
-    </Router>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      className="navbar-custom"
+      variant="dark"
+      // sticky="top"
+    >
+      <Link to="/spatepate/">
+        <Navbar.Brand className="navbar-brand">spatepate</Navbar.Brand>
+      </Link>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Link className="nav-link" to="/spatepate/">
+            Home
+          </Link>
+          <Link className="nav-link" to="/spatepate/programming">
+            <i className="fa fa-code"></i> Programming
+          </Link>
+          <Link className="nav-link" to="/spatepate/developers">
+            Developers
+          </Link>
+          <Link className="nav-link" to="/spatepate/about-us">
+            About
+          </Link>
+          <Link className="nav-link" to="/spatepate/contact-us">
+            Contact
+          </Link>
+        </Nav>
+        <Nav>
+          <Link className="nav-link" to="/spatepate/resources">
+            Resources
+          </Link>
+          <Nav.Link
+            eventKey={2}
+            href="https://www.reddit.com/r/dankmemes/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Dank memes
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
-}
-
-
+};
 
 export default NavBar;
-  
